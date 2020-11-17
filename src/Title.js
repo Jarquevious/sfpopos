87eded42b634b3b54bfb837dbb7bfefe6ec4650d
Jarquevious <jarquevious@gmail.com>
2020-11-16 16:54:36 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,16 @@
+// src/Title.js
+import './Title.css';
+
+import React from 'react'
+
+function Title() {
+    return (
+      <div className="Title">
+        <header>
+          <h1>SFPOPOS</h1>
+          <div className="Title-Subtitle"><h3>San Francisco Privately Owned Public Open Spaces</h3></div>
+        </header>
+      </div>
+    )
+  }
+export default Title
