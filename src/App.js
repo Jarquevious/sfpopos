1239d8463a148b5a9b5bede36667ceed9580c090
Jarquevious <jarquevious@gmail.com>
2020-11-19 14:13:38 -0500

    content component built

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 // src/App.js
 
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
-
-
+import POPOSList from './POPOSList';
 
 
 function App() {
   return (
     <div className = 'App'>
       <Title />
+      <POPOSList />
     </div>
   );
 }
