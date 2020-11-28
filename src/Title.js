f39f1f9cb997362af29d4b105fc8f603aac986ad
Jarquevious <jarquevious@gmail.com>
2020-11-27 20:02:54 -0500

    active link added

--- src/Title.js
+++ src/Title.js
@@ -9,6 +9,21 @@ function Title() {
         <header>
           <h1>SFPOPOS</h1>
           <div className="Title-Subtitle"><h3>San Francisco Privately Owned Public Open Spaces</h3></div>
+        
+        <div>
+        <NavLink
+          className="nav-link"
+          activeClassName="nav-link-active"
+          exact
+          to="/">List</NavLink>
+
+        <NavLink
+          className="nav-link"
+          activeClassName="nav-link-active"
+          to="/about">About</NavLink>
+        
+        </div>
+
         </header>
       </div>
     )
